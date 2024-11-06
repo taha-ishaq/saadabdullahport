25ee5de1039481bbe627c7b6832532bcf5811f7e
taha-ishaq <tahaishaq7346@gmail.com>
2024-11-06 22:45:27 +0500

    dine

---
+++ src/app/components/AboutSection.jsx
@@ -0,0 +1,148 @@
+"use client";
+import React, { useTransition, useState } from "react";
+import Image from "next/image";
+import TabButton from "./TabButton";
+import Link from "next/link";
+
+const TAB_DATA = [
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Binance Angel aspirant</li>
+        <li>AI</li>
+        <li> digital marketing</li>
+        <li>video editing</li>
+        <li>Content creator </li>
+        <li>data science</li>
+        <li> crypto
+        Passionate</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>BSCS Thal University</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Certifications",
+    id: "certifications",
+    content: (
+      <ul className="list-disc pl-2">
+         <Link
+              href="/cert1.pdf"
+              className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
+            >
+              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+              Standford machine learning
+              </span>
+            </Link>
+            <Link
+              href="/cert2.pdf"
+              className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
+            >
+              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+              IBM data base
+              </span>
+            </Link>
+            <Link
+              href="/cert3.pdf"
+              className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
+            >
+              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+              Data science and ai
+              </span>
+            </Link>
+            <Link
+              href="/cert4.pdf"
+              className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
+            >
+              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+              Unsupervised machine learning Standford
+              </span>
+            </Link>
+            <Link
+              href="/cert5.pdf"
+              className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
+            >
+              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+              Internship completion certificate
+              </span>
+            </Link>
+       
+      </ul>
+    ),
+  },
+];
+
+const AboutSection = () => {
+  const [tab, setTab] = useState("skills");
+  const [isPending, startTransition] = useTransition();
+
+  const handleTabChange = (id) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
+
+  return (
+    <section className="text-white" id="about">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+        <Image
+          src="/images/about-image.png"
+          width={500}
+          height={500}
+          alt="A description of the image"
+        />
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+          <p className="text-base lg:text-lg">
+            I&apos;m a passionate crypto trader and data scientist with expertise in AI, digital marketing, and video editing. I create content that is engaging, educational, and easy to understand, with a focus on data science and cryptocurrency. I aim to balance my academic journey with my professional experiences to thrive in a competitive world.
+            Personal Aspirations
+Aspires to serve as an ambassador for different cultures
+Promoting humanism in Pakistan
+Aiming to portray a peaceful image of Pakistan through my scholarship program
+Volunteer Roles
+Member of VLG (Volunteer Leadership Group)
+Intermediary between citizens and the government
+Focused on improving public service delivery
+Guiding the public to access relevant departments and raise their concerns
+Engaged in monitoring and improving governance measures
+
+          </p>
+          <div className="flex flex-row justify-start mt-8">
+            <TabButton
+              selectTab={() => handleTabChange("skills")}
+              active={tab === "skills"}
+            >
+              Skills
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("education")}
+              active={tab === "education"}
+            >
+              Education
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("certifications")}
+              active={tab === "certifications"}
+            >
+              Certifications
+            </TabButton>
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab).content}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
